3e267243acdafb82efb33f754530db277a91a70e
Kalladka <155704392+Kalladka@users.noreply.github.com>
2024-01-05 09:50:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+class Main_App(MDApp):
+if
+def build(self):
+return MDLabel (text="Welcome to Marnami Tech",halign="cent")
+name == '__main__':
+Main_App().run()
